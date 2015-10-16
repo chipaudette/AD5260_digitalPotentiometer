aa42cb5f3b6237352fce78de79269364f7b1e58e
Chip Audette <chipaudette@yahoo.com>
2015-10-16 07:29:36 -0400

    First Commit.

---
+++ AD5260_digitalPotentiometer.ino
@@ -0,0 +1,70 @@
+/*
+  Digital Pot Control
+  
+  This example controls an Analog Devices AD5260 digital potentiometer.
+  The AD5260 has 1 potentiometer channel. Each channel's pins are labeled
+  A - connect this to voltage
+  W - this is the pot's wiper, which changes when you set it
+  B - connect this to ground.
+ 
+ The AD5260 is SPI-compatible,and to command it, you send one byte
+ with the resistance value (0 - 255).  
+ 
+ The circuit:
+  * A pin of AD5260 connected to +5V
+  * B pin of AD5260 connected to ground
+  * An LED and a 220-ohm resisor in series connected from each W pin to ground
+  * CS - to digital pin 10  (SS pin)
+  * SDI - to digital pin 11 (MOSI pin)
+  * CLK - to digital pin 13 (SCK pin)
+
+ created 04 Oct 2015
+ by Chip Audette
+
+ based on Arduino Example AD5206 by Tom Igoe
+  
+*/
+
+
+// include the SPI library:
+#include <SPI.h>
+
+
+// set pin 10 as the slave select for the digital pot:
+const int slaveSelectPin = 10;
+
+void setup() {
+  
+  //start serial for debugging
+  Serial.begin(115200);
+  
+  // set the slaveSelectPin as an output:
+  pinMode (slaveSelectPin, OUTPUT);
+  pinMode (slaveSelectPin, HIGH);
+  
+  // initialize SPI:
+  SPI.begin(); 
+  SPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));
+}
+
+void loop() {
+
+  // change the resistance from min to max:
+  for (int level = 0; level < 255; level += 25) {
+    Serial.print("Commanding value: ");
+    Serial.print(level);
+    Serial.print(", Should Indicate ");
+    Serial.print(((float)level)/255.0*5.0);
+    Serial.println(" V");
+
+    //send value via SPI
+    digitalWrite(slaveSelectPin,LOW);// take the SS pin low to select the chip
+    SPI.transfer(level);//  send value via SPI:
+    digitalWrite(slaveSelectPin,HIGH);// take the SS pin high to de-select the chip
+
+    //wait to allow measurement
+    delay(3000);  //delay X seconds
+  }
+}
+
+
